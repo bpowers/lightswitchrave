62a950de238b342dde1d3b89e99cd3462980e87b
Bobby Powers <bobbypowers@gmail.com>
2012-10-02 15:33:12 -0400

    proxy: fix proxying of queries with '#' and '?'

--- proxy.go
+++ proxy.go
@@ -4,12 +4,15 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func proxyToCnote(w http.ResponseWriter, r *http.Request) {
 	// don't cache any API responses
 	forceRevalidate(w)
 	path := r.URL.Path[len("/api/"):]
+	path = strings.Replace(path, "#", "%23", -1)
+	path = strings.Replace(path, "?", "%3F", -1)
 	uri := "http://127.0.0.1:1969/" + path
 	resp, err := http.Get(uri)
 	if err != nil {
