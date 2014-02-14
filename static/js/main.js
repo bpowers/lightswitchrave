8d696112b01cd9f900c03a83f9c539619d2ae6a4
Bobby Powers <bobbypowers@gmail.com>
2014-02-13 22:52:36 -0500

    refactor backbone project to be much more sane

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-$(function() {
+(function() {
     // ---------------------------------------------------------- utils --- //
     var escapePath = function(path) {
         path = decodeURIComponent(path);
@@ -83,15 +83,19 @@ $(function() {
 
     var TrackList = Backbone.Collection.extend({
         model: Track,
+        initialize: function(args) {
+            this.collections = args.collections;
+        },
         load: function(m) {
+            var collections = this.collections;
             // clear any existing highlighted artist or album
-            for (k in Collections) {
-                if (Collections[k].curr) {
-                    Collections[k].curr.set('selected', false);
-                    Collections[k].curr = null;
+            for (k in collections) {
+                if (collections[k].curr) {
+                    collections[k].curr.set('selected', false);
+                    collections[k].curr = null;
                 }
             }
-            Collections[m.get('type')].curr = m;
+            collections[m.get('type')].curr = m;
             m.set('selected', true);
             this.url = 'api/' + m.get('type') + '/' + encodeURIComponent(m.get('name'));
             this.fetch();
@@ -102,21 +106,17 @@ $(function() {
     var CategoryView = Backbone.View.extend({
         tagName: 'div',
         template: _.template($('#list-template').html()),
-
         events: {
             'click a': 'click'
         },
-
-        initialize: function() {
+        initialize: function(args) {
+            this.tracks = args.tracks;
             this.model.bind('change', this.render, this);
         },
-
         curr: null,
-
         click: function(e) {
-            Tracks.load(this.model);
+            this.tracks.load(this.model);
         },
-
         render: function() {
             this.$el.html(this.template(this.model.toJSON()));
             return this;
@@ -126,23 +126,20 @@ $(function() {
     var TrackView = Backbone.View.extend({
         tagName: 'div',
         template: _.template($('#track-template').html()),
-
         events: {
             'click a.track': 'play'
         },
-
-        initialize: function() {
+        initialize: function(args) {
+            this.nowPlaying = args.nowPlaying;
             this.model.bind('change', this.render, this);
         },
-
         render: function() {
             this.$el.html(this.template(this.model.toJSON()));
             return this;
         },
-
         play: function(e) {
             e.preventDefault();
-            NowPlaying.start(this.model);
+            this.nowPlaying.start(this.model);
         }
     });
 
@@ -158,10 +155,10 @@ $(function() {
         prev: null,
         curr: null,
 
-        initialize: function() {
+        initialize: function(args) {
+            this.tracks = args.tracks;
             this.$el.hide();
         },
-
         start: function(track, startPaused) {
             if (this.curr) {
                 this.prev = this.curr;
@@ -195,17 +192,17 @@ $(function() {
             var found = false;
             var i;
             for (i in Tracks.models) {
-                if (this.curr === Tracks.models[i]) {
+                if (this.curr === this.tracks.models[i]) {
                     found = true;
                     if (i > 0)
                         i--;
                     break;
                 }
             }
-            if (!found && Tracks.models.length) {
+            if (!found && this.tracks.models.length) {
                 // didn't find it.  must have switched categories.
                 // start playing the first track.
-                this.start(Tracks.models[0], !this.shouldPlay);
+                this.start(this.tracks.models[0], !this.shouldPlay);
                 return true;
             } else {
                 // found one. play it.
@@ -220,21 +217,21 @@ $(function() {
         playNext: function() {
             var found = false;
             var i;
-            for (i in Tracks.models) {
-                if (this.curr === Tracks.models[i]) {
+            for (i in this.tracks.models) {
+                if (this.curr === this.tracks.models[i]) {
                     found = true;
                     i++;
                     break;
                 }
             }
-            if (!found && Tracks.models.length) {
+            if (!found && this.tracks.models.length) {
                 // didn't find it.  must have switched categories.
                 // start playing the first track.
-                this.start(Tracks.models[0], !this.shouldPlay);
+                this.start(this.tracks.models[0], !this.shouldPlay);
                 return true;
-            } else if (i < Tracks.models.length) {
+            } else if (i < this.tracks.models.length) {
                 // found one. play it.
-                this.start(Tracks.models[i], !this.shouldPlay);
+                this.start(this.tracks.models[i], !this.shouldPlay);
                 return true;
             }
             return false;
@@ -254,12 +251,9 @@ $(function() {
     });
 
     var AppView = Backbone.View.extend({
-
         el: $('#content'),
-
         initial: {},
-
-        initialize: function() {
+        initialize: function(args) {
             // check to see if we are loading a URL with an
             // already-selected album or artist
             var hash = location.hash.substring(1);
@@ -271,53 +265,47 @@ $(function() {
                 }
             }
 
-            Artists.bind('add', this.addOne, this);
-            Artists.bind('reset', this.addAllArtists, this);
+            this.artists = args.artists;
+            this.albums = args.albums;
+            this.tracks = args.tracks;
+            this.nowPlaying = args.nowPlaying;
 
-            Albums.bind('add', this.addOne, this);
-            Albums.bind('reset', this.addAllAlbums, this);
+            this.artists.bind('add', this.addOne, this);
+            this.artists.bind('reset', this.addAllArtists, this);
 
-            Tracks.bind('add', this.addTrack, this);
-            Tracks.bind('reset', this.addAllTracks, this);
+            this.albums.bind('add', this.addOne, this);
+            this.albums.bind('reset', this.addAllAlbums, this);
 
-            Artists.fetch();
-            Albums.fetch();
+            this.tracks.bind('add', this.addTrack, this);
+            this.tracks.bind('reset', this.addAllTracks, this);
+
+            this.artists.fetch();
+            this.albums.fetch();
         },
         addOne: function(a) {
-            var view = new CategoryView({model: a});
+            var view = new CategoryView({model: a, tracks: this.tracks});
             var type = a.get('type');
             $('#' + type).append(view.render().el);
-            if (type in App.initial && App.initial[type] === a.get('name')) {
-                Tracks.load(a);
-                App.initial = {};
+            if (type in this.initial && this.initial[type] === a.get('name')) {
+                this.tracks.load(a);
+                this.initial = {};
             }
         },
         addAllArtists: function() {
-            Artists.each(this.addOne);
+            this.artists.each(this.addOne, this);
         },
         addAllAlbums: function() {
-            Albums.each(this.addOne);
+            this.albums.each(this.addOne, this);
         },
         addTrack: function(t) {
-            var view = new TrackView({model: t});
+            var view = new TrackView({model: t, nowPlaying: this.nowPlaying});
             $('#tracks').append(view.render().el);
         },
         addAllTracks: function() {
             $('#tracks').empty();
-            Tracks.each(this.addTrack);
+            this.tracks.each(this.addTrack, this);
         }
     });
-
-    // ---------------------------------------------------------- state --- //
-    var NowPlaying = new NowPlayingView;
-    var Artists = new ArtistList;
-    var Albums = new AlbumList;
-    var Tracks = new TrackList;
-    var Collections = {
-        'artist': Artists,
-        'album': Albums
-    };
-    var App = new AppView;
 
     // constants
     var codeSpace = ' '.charCodeAt();
@@ -325,24 +313,46 @@ $(function() {
     var codeArrowLeft = 37;
 
     // ------------------------------------------------- initialization --- //
-    $(document).bind('keyup', function(e) {
-        if (e.which === codeArrowRight) {
-            e.preventDefault();
-            NowPlaying.playNext();
-        } else if (e.which === codeArrowLeft) {
-            e.preventDefault();
-            NowPlaying.playPrev();
-        } else if (e.which === codeSpace) {
-            e.preventDefault();
-            if (NowPlaying.curr)
-                NowPlaying.togglePause();
-            else
-                NowPlaying.playNext();
+    $(function() {
+        var artists = new ArtistList();
+        var albums = new AlbumList();
+        var collections = {
+            'artist': artists,
+            'album': albums
+        };
+        var tracks = new TrackList({collections: collections});
+        var nowPlaying = new NowPlayingView({tracks: tracks});
+        var app = new AppView({
+            artists: artists,
+            albums: albums,
+            tracks: tracks,
+            nowPlaying: nowPlaying,
+        });
+
+        // global key handlers for playback control
+        $(document).bind('keyup', function(e) {
+            switch(e.which) {
+            case codeArrowRight:
+                e.preventDefault();
+                nowPlaying.playNext();
+                break;
+            case codeArrowLeft:
+                e.preventDefault();
+                nowPlaying.playPrev();
+                break;
+            case codeSpace:
+                e.preventDefault();
+                if (nowPlaying.curr)
+                    nowPlaying.togglePause();
+                else
+                    nowPlaying.playNext();
+                break;
+            }
+        });
+
+        // warn if using firefox, because it doesn't have aac or mp3 codecs
+        if (navigator.userAgent.indexOf('Chrome') < 0) {
+            $('#footer').append('<span style="color: red; font-weight: bold;">(Firefox can\'t play aac or mp3 files.  Use Chrome.)</span>');
         }
     });
-
-    // warn if using firefox, because it doesn't have aac or mp3 codecs
-    if (navigator.userAgent.indexOf('Chrome') < 0) {
-        $('#footer').append('<span style="color: red; font-weight: bold;">(Firefox can\'t play aac or mp3 files.  Use Chrome)</span>');
-    }
-})
+})();
